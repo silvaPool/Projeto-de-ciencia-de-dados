a566e1d29266e58f4242fb2220c8197082239ecd
silvaPool <silva.friends22@gmail.com>
2025-07-13 19:07:10 -0300

    Atualizacao e implementacacao do scikit-learn

--- historicofinanceiro.py
+++ historicofinanceiro.py
@@ -57,22 +57,166 @@ pay_1_bins = np.array(range(-2,10)) - 0.5
 
 #adicionando rótulos aos eixos
 
-df_clean_2[pay_feats[0]].hist(bins=pay_1_bins)
+#df_clean_2[pay_feats[0]].hist(bins=pay_1_bins)
 
-plt.xlabel('PAY_1')
+#plt.xlabel('PAY_1')
 
-plt.ylabel('Number of accounts')
+#plt.ylabel('Number of accounts')
 
 #plt.show()
 
 #subplotagens
 
-mpl.rcParams['font.size'] = 4
+#mpl.rcParams['font.size'] = 4
 
-df_clean_2[pay_feats].hist(bins=pay_1_bins, layout=(2,3))
+#df_clean_2[pay_feats].hist(bins=pay_1_bins, layout=(2,3))
 
 #plt.show()
 
 # verificando os meses de agosto e julho
 
-print(df_clean_2.loc[df_clean_2['PAY_2'] == 2, ['PAY_2', 'PAY_3']].head())
+#print(df_clean_2.loc[df_clean_2['PAY_2'] == 2, ['PAY_2', 'PAY_3']].head())
+
+#proporção da classe positiva
+#print(df_clean_2['default payment next month'].mean())
+
+#verificação do número de amostras em cada classe
+
+#print(df_clean_2.groupby('default payment next month')['ID'].count())
+
+from sklearn.linear_model import LogisticRegression
+
+my_lr = LogisticRegression()
+
+#print(my_lr)
+
+my_new_lr = LogisticRegression(C=1.0, class_weight=None, dual=False,
+                               fit_intercept=True,
+                               intercept_scaling=1, max_iter=100, multi_class='auto',
+                               n_jobs=None, penalty='l2', random_state=None, solver='warn',
+                               tol=0.0001, verbose=0, warm_start=False)
+
+my_new_lr.C = 0.1
+my_new_lr.solver = 'liblinear'
+
+#print(my_new_lr)
+
+# 10 primeiras amostras de uma única característica
+
+X = df_clean_2['EDUCATION'][0:10].values.reshape(-1,1)
+
+#print(X)
+
+# 10 valores correspondentes da variável de resposta
+
+Y = df_clean_2['default payment next month'][0:10].values
+
+#print(Y)
+
+# agora o objeto de modelo my_new_lr é um modelo treinado
+
+my_new_lr.fit(X, Y)
+
+# novas características para as quais serão feitas previsões
+
+new_X = df_clean_2['EDUCATION'][10:20].values.reshape(-1,1)
+
+#print(new_X)
+
+# as previsões são feitas assim
+
+#print(my_new_lr.predict(new_X))
+
+# visualização de quais são os valores reais correspondentes a essas previsões
+
+#print(df_clean_2['default payment next month'][10:20].values)
+
+# Gerando dados sintéticos
+np.random.seed(seed=1)
+
+X = np.random.uniform(low=0.0, high=10.0, size=(1000,))
+
+#print(X[0:10])
+
+#criando dados da regressão linear
+
+np.random.seed(seed=1)
+
+#variável de inclinação
+
+slope = 0.25
+
+#variável de intercepção
+
+intercept = -1.25
+
+# variável de resposta
+
+y = slope * X + np.random.normal(loc=0.0, scale=1.0, size=(1000,)) + intercept
+
+mpl.rcParams['figure.dpi'] = 400
+#plt.scatter(X, y, s=1)
+#plt.show()
+
+# Regressão linear com o Scikit-Learn
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+
+lin_reg.fit(X.reshape(-1,1), y)
+#print(lin_reg.intercept_)
+#print(lin_reg.coef_)
+
+y_pred = lin_reg.predict(X.reshape(-1,1))
+
+plt.scatter(X, y, s=1)
+plt.plot(X, y_pred, 'r')
+#plt.show()
+
+# dados de treinamento e dados de teste
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df_clean_2['EDUCATION'].values.reshape(-1,1), df_clean_2['default payment next month'].values,
+    test_size=0.2, random_state=24)
+
+#dimensão dos conjuntos de treinamento e teste
+
+#print(X_train.shape)
+#print(X_test.shape)
+#print(y_train.shape)
+#print(y_test.shape)
+
+# Frações das classes nos dados de treinamento e teste
+
+#print(np.mean(y_train))
+#print(np.mean(y_test))
+
+# exemplo de modelo para ilustrar métricas de classificação binária
+
+from sklearn.linear_model import LogisticRegression
+
+example_lr = LogisticRegression(C=0.1, class_weight=None, dual=False, fit_intercept=True,
+                                intercept_scaling=1, max_iter=100, multi_class='auto',
+                                n_jobs=None, penalty='l2', random_state=None, solver='liblinear',
+                                tol=0.0001, verbose=0, warm_start=False)
+
+example_lr.fit(X_train, y_train)
+
+y_pred = example_lr.predict(X_test)
+
+# Calculando a acurácia da classificação com uma máscara lógica
+
+is_correct = y_pred == y_test
+
+#print(np.mean(is_correct))
+
+# Calculando a acurácia da classificação com o scikit-learn
+
+print(example_lr.score(X_test, y_test))
+
+from sklearn import metrics
+
+print(metrics.accuracy_score(y_test, y_pred))
